9d21e52188fb58e6c900aadfe039f80b87abfa27
Nil Monfort <nil.monfort@estudiants.urv.cat>
2022-10-08 15:47:49 +0200

    msg alert

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
 
-const Header = () => {
+const Header = ({ presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto }) => {
   return (
     <header>
         <h1>Planificador de Gastos</h1>
-        <NuevoPresupuesto/>
+        <NuevoPresupuesto 
+            presupuesto={presupuesto}  
+            setPresupuesto={setPresupuesto}
+            isValidPresupuesto={isValidPresupuesto}
+            setIsValidPresupuesto={setIsValidPresupuesto}
+        />
     
     </header>
 
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+
+    const [mensaje, setMensaje] = useState('')
+
+    const handlePressupuesto = (e) => {
+        e.preventDefault();
+        if(!(presupuesto) || (presupuesto) < 0){
+            setMensaje('No es un presupuesto valido')
+            return
+        } 
+        setMensaje('')
+
+        console.log(mensaje);
+    }
+
+
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
         <form className='formulario'>
             <div className="campo">
                 <label> Definir Presupuesto</label>
-                <input className='nuevo-presupuesto' type="text" placeholder='Añade tu Presupuesto' />
+                <input className='nuevo-presupuesto' type="number" placeholder='Añade tu Presupuesto' value={presupuesto} onChange={ (e) => setPresupuesto(Number(e.target.value))} />
             </div>
-            <input type="submit" value="Añadir" />
+            <input type="submit" value="Añadir" onClick={handlePressupuesto} />
+            {mensaje && <Mensaje tipo="error" >{mensaje}</Mensaje> }
         </form>
 
 
